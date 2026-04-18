cb3512f74ce67019d0f550a890617e4f1e6c8058
Ilya Kolpakov <ilyako96@gmail.com>
2026-04-18 21:41:53 +0300

    Initial Commit

---
+++ Listen Here/Listen_HereApp.swift
@@ -0,0 +1,17 @@
+//
+//  Listen_HereApp.swift
+//  Listen Here
+//
+//  Created by qweq on 18.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Listen_HereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
